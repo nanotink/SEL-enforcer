51829b6dca8c3070e02b9fad272369c9a05c0590
nanotink <xdnoname366@gmail.com>
2025-05-08 13:33:24 +0200

    Notify user to reboot

--- action.sh
+++ action.sh
@@ -1,3 +1,4 @@
 resetprop --delete persist.log.tag.LSPosed
 resetprop --delete persist.log.tag.LSPosed-Bridge
-resetprop --delete ro.kernel.qemu
+resetprop --delete ro.kernel.qemu #May not work!
+echo "Please reboot🔄 to apply changes!"
