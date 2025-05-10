f794e08c16b41a959904c033ffb758dd21172de2
nanotink <xdnoname366@gmail.com>
2025-05-11 00:51:18 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,4 +1,11 @@
+#fix non-sdk
+su -c settings delete global hidden_api_policy
+su -c settings delete global hidden_api_policy_p_apps
+su -c settings delete global hidden_api_policy_pre_p_apps
+#fix 'Evil service(2)' (Native test)
 resetprop --delete persist.log.tag.LSPosed
 resetprop --delete persist.log.tag.LSPosed-Bridge
+#hide *emulator*
 resetprop --delete ro.kernel.qemu #May not work!
+#notification
 echo "Please reboot🔄 to apply changes!"
